8b00c6b5120bff96bc8dea48c353962ad10b5726
wywn <85445193+wywn@users.noreply.github.com>
2024-01-28 18:40:23 +0800

    Create antiAD.js

---
+++ antiAD.js
@@ -0,0 +1,6 @@
+if (window.location.host == "wyywn.qq.us.kg"){
+    var i = setInterval(function(){
+        document.querySelector("body > cloudflare-app").remove();
+        clearInterval(i);
+    },100);
+}
