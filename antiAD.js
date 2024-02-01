b6a8a01208880a286e2910395dd3e22c378e0bbe
wywn <85445193+wywn@users.noreply.github.com>
2024-02-01 10:18:57 +0800

    Update antiAD.js

--- antiAD.js
+++ antiAD.js
@@ -1,6 +1,19 @@
-if (window.location.host == "wyywn.qq.us.kg"){
+var res = "";
+var isMatched = false;
+var local = window.location.host;
+var arr = local.split(".");
+var target = "qq.us.kg";
+for (var i = 0;i < arr.length;i++){
+    res = arr[arr.length-i-1] + res;
+    if (res == target){
+        isMatched = true;
+        break;
+    }
+    res = "." + res;
+}
+if (isMatched){
     var i = setInterval(function(){
         document.querySelector("body > cloudflare-app").remove();
         clearInterval(i);
     },100);
-} 
+}
